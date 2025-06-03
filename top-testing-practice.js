bf3afa9af635637276cfa2ce03b5b7d3efeb71cb
Jordan Adams <jordan.adams@apexservicemw.com>
2025-06-03 08:27:01 -0400

    project init and initial commit

---
+++ top-testing-practice.js
@@ -0,0 +1,22 @@
+export function capitalize(str) {
+  // implementation
+}
+
+export function reverseString(str) {
+  // implementation
+}
+
+export const calculator = {
+  add(a, b) { /* implementation */ },
+  subtract(a, b) { /* implementation */ },
+  divide(a, b) { /* implementation */ },
+  multiply(a, b) { /* implementation */ }
+};
+
+export function caesarCipher(str, shift) {
+  // implementation
+}
+
+export function analyzeArray(arr) {
+  // implementation
+}
---
+++ top-testing-practice.test.js
@@ -0,0 +1,5 @@
+import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from './top-testing-practice.js';
+
+test('capitalize capitalizes the first letter', () => {
+  // example test
+});
